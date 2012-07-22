9fb4514e7c542cfd2e5da350738c958e2a627a40
Paul Payne <paul@payne.io>
2012-07-21 23:56:07 -0700

    Don't use getent when on OSX.

--- bash.d/misc_config.sh
+++ bash.d/misc_config.sh
@@ -2,7 +2,9 @@
 
 # Get my name from the system to make it easily available everywhere I might
 # need it
-export MYFULLNAME=$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)
+if [[ "$( type -t 'getent' )" == 'function' ]]; then
+  export MYFULLNAME=$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)
+fi
 
 # an argument to the cd builtin command that is not a directory is assumed to
 # be the name of a variable whose value is the directory to change to.
