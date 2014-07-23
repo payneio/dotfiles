701ab7b68d5772f77f700c83024ff64b56b290ae
Paul Payne <paul@payne.io>
2014-07-22 22:50:49 -0700

    Check for brew before trying to use it.

--- bash.d/brew_git_completion.sh
+++ bash.d/brew_git_completion.sh
@@ -1,3 +1,5 @@
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-      . `brew --prefix`/etc/bash_completion
+if hash brew 2>/dev/null; then
+  if [ -f `brew --prefix`/etc/bash_completion ]; then
+        . `brew --prefix`/etc/bash_completion
+  fi
 fi
