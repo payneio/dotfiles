b2ac639231ebde2a6fd8a279160c7de1539ae2ea
Paul Payne <paul@payne.io>
2014-10-21 14:59:22 -0700

    Move homebrew git bash completion into homebrew.sh.

--- bash.d/brew_git_completion.sh
+++
@@ -1,9 +0,0 @@
-if hash brew 2>/dev/null; then
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-        . `brew --prefix`/etc/bash_completion
-  fi
-  if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
-        source `brew --prefix`/etc/bash_completion.d/git-prompt.sh
-  fi
-fi
-
--- bash.d/homebrew.sh
+++ bash.d/homebrew.sh
@@ -1,2 +1,15 @@
-pathprepend "/usr/local/bin"
-#export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+if hash brew 2>/dev/null; then
+
+  pathprepend "/usr/local/bin"
+  #export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
+
+  if [ -f `brew --prefix`/etc/bash_completion ]; then
+        . `brew --prefix`/etc/bash_completion
+  fi
+  if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+        source `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+  fi
+
+  echo "brew ready."
+
+fi
