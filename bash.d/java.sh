c357b6629d1c9d85581ef33de586d3cdde147c1e
Paul Payne <paul@payne.io>
2014-12-10 09:09:13 -0800

    bash.d: Java intialization.

---
+++ bash.d/java.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+java_dir="/Library/Java/JavaVirtualMachines/jdk1.7.0_60.jdk/Contents/Home"
+if [ -d $java_dir ]; then
+  export JAVA_HOME=$java_dir
+  echo "JAVA_HOME=$java_dir"
+fi
+
+groovy_dir="/usr/local/opt/groovy/libexec"
+if [ -d $groovy_dir ]; then
+  export GROOVY_HOME=$groovy_dir
+  echo "GROOVY_HOME=$groovy_dir"
+fi
+
