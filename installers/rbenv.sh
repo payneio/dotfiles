5515281594ef181b9ba1901527bc2848751ebb05
Paul Payne <paul@payne.io>
2015-01-03 23:41:05 -0800

    Make some installations easier.

---
+++ installers/rbenv.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo "rbenv installed. Remember to reloadsh"
+
