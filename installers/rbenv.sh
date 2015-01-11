e7d8dbadc9db260e191a1121e9b698c5a515d75b
Paul Payne <paul@payne.io>
2015-01-10 20:19:25 -0800

    Add an rbenv installer (for ubuntu).

--- installers/rbenv.sh
+++ installers/rbenv.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 echo "rbenv installed. Remember to reloadsh"
 
