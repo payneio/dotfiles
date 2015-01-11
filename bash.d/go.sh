0ca604712eeaaa03bf7f258f537742d69332cf3d
Paul Payne <paul@payne.io>
2015-01-10 20:17:33 -0800

    Add go bin to path on linux.

--- bash.d/go.sh
+++ bash.d/go.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-pathappend "/usr/local/go/bin"
+go="/usr/local/go"
+if [ -d $go ]; then
+  pathappend "$go/bin"
+  echo "Go ready."
+fi
+
 go_path="$HOME/go"
 if [ -d $go_path ]; then
   export GOPATH=$go_path
   pathappend "$go_path/bin"
   alias gos="cd $go_path/src"
-  echo "Go ready."
 fi
+
