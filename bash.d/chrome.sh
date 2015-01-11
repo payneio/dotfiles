bacbade285995d9d6cd5439ba0dc95c71abd70d0
Paul Payne <paul@payne.io>
2015-01-10 20:15:46 -0800

    Allow Created new window in existing browser session. to open on linux as well as osx.

--- bash.d/chrome.sh
+++ bash.d/chrome.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# osx
 if [ -e "/Applications/Google Chrome.app/" ]; then
   function chrome() { open -a "Google Chrome" "$1"; }
   function google() { open -a "Google Chrome" "http://www.google.com/search?q=$1"; }
 fi
+
+# ubuntu
+if hash google-chrome 2>/dev/null; then
+  alias chrome='google-chrome'
+fi
+
