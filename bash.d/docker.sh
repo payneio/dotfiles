68629a659570f713370fa8e017576b960cf0bcba
Paul Payne <paul@payne.io>
2015-07-20 14:05:15 -0700

    Boot2Docker setup for Docker in OSX.

--- bash.d/docker.sh
+++ bash.d/docker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-
-case $OSTYPE in 
+case $OSTYPE in
   darwin*)
     ip_file="$HOME/.vars/docker_host_ip"
     if hash docker 2>/dev/null && [ -f $IP_FILE ]; then
+
+      export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+      export DOCKER_TLS_VERIFY=1
       export DOCKER_HOST=tcp://$(< "$ip_file")
+
       echo "docker ready."
     fi
-    ;; 
+    ;;
   linux*)
     if hash docker 2>/dev/null && [ -f $IP_FILE ]; then
       echo "docker ready."
