fbaa3718a8a3a36c53deab1aa5302d8cc14a6333
Paul Payne <paul@payne.io>
2016-04-30 21:04:22 -0700

    Adds FLEETCTL_ENDPOINT to fleet config

--- bash.d/fleet.sh
+++ bash.d/fleet.sh
@@ -3,6 +3,7 @@
 ip_file="$HOME/.vars/fleetctl_tunnel_ip"
 if hash fleetctl 2>/dev/null && [ -f $ip_file ]; then
   export FLEETCTL_TUNNEL=$(< $ip_file)
+  export FLEETCTL_ENDPOINT=http://127.0.0.1:4001
   export FLEETCTL_STRICT_HOST_KEY_CHECKING=false
   alias fc='fleetctl'
   alias fcj='fleetctl journal'
