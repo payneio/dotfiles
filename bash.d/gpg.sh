f9b6c443a6c5ee41f5306fe3c4302a9d786c1d50
Paul Payne <paul@payne.io>
2016-04-30 21:07:05 -0700

    Adds GPG config

---
+++ bash.d/gpg.sh
@@ -0,0 +1,18 @@
+case $OSTYPE in
+  linux*)
+    # export GPG_TTY=$(tty)
+    # if [ -f "${HOME}/.gpg-agent-info" ]; then
+    #   . "${HOME}/.gpg-agent-info"
+    #   export GPG_AGENT_INFO
+    # fi
+    if test -f $HOME/.gpg-agent-info && \
+      kill -0 `cut -d: -f 2 $HOME/.gpg-agent-info` 2>/dev/null; then
+        GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info`
+        export GPG_AGENT_INFO
+    else
+      eval `gpg-agent --daemon --write-env-file`
+    fi
+    export GPG_TTY=tty
+    echo "gpg ready."
+  ;;
+esac
