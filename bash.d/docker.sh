3e2305a48a6247fd288be61d2fd57d7c1b514d8f
Paul Payne <paul@payne.io>
2017-05-17 14:25:46 -0700

    Updates docker to docker-machine

--- bash.d/docker.sh
+++ bash.d/docker.sh
@@ -3,12 +3,8 @@
 case $OSTYPE in
   darwin*)
     ip_file="$HOME/.vars/docker_host_ip"
-    if hash docker 2>/dev/null && [ -f $IP_FILE ]; then
-
-      export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-      export DOCKER_TLS_VERIFY=1
-      export DOCKER_HOST=tcp://$(< "$ip_file")
-
+    if hash docker-machine 2>/dev/null && [ -f $IP_FILE ]; then
+      eval `docker-machine env`
       echo "docker ready."
     fi
     ;;
