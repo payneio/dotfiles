2dcd7de8c63d43912126e5527d9b049586baf462
Paul Payne <paul@payne.io>
2017-05-17 14:26:03 -0700

    Adds python conda

--- bash.d/python.sh
+++ bash.d/python.sh
@@ -1,1 +1,12 @@
-pathappend "$HOME/.local/bin"
+pip_bin="$HOME/.local/bin"
+if [ -d $pip_bin ]; then
+  pathappend "$HOME/.local/bin"
+  echo "python pip ready."
+fi
+
+conda="$HOME/miniconda3"
+if [ -d $conda ]; then
+  pathprepend "$HOME/miniconda3/bin"
+  echo "python conda ready."
+fi
+
