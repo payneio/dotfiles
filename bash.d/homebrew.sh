f97d47de29e3083b037dcb5ae64a6dcb811ea1b9
Paul Payne <paul@payne.io>
2017-05-17 14:26:27 -0700

    Updates homebrew

--- bash.d/homebrew.sh
+++ bash.d/homebrew.sh
@@ -1,6 +1,7 @@
 if hash brew 2>/dev/null; then
 
   pathprepend "/usr/local/bin"
+  pathprepend "/usr/local/sbin"
   #export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
 
   if [ -f `brew --prefix`/etc/bash_completion ]; then
