7fc18068d39b228a3b8a09b26cc405be894e595e
Paul Payne <paul@payne.io>
2017-12-13 13:35:57 -0800

    Fixes google cloud setup

--- bash.d/google_cloud.sh
+++ bash.d/google_cloud.sh
@@ -5,13 +5,13 @@ GCLOUD_HOME="$HOME/google-cloud-sdk"
 if [ -d $GCLOUD_HOME ]; then
 
     # The next line updates PATH for the Google Cloud SDK.
-    if [ -f '$HOME/google-cloud-sdk/path.bash.inc' ]; then
-        source '$HOME/google-cloud-sdk/path.bash.inc'
+    if [ -f "$GCLOUD_HOME/path.bash.inc" ]; then
+        source "$GCLOUD_HOME/path.bash.inc"
     fi
 
     # The next line enables shell command completion for gcloud.
-    if [ -f '$HOME/google-cloud-sdk/completion.bash.inc' ]; then
-        source '$HOME/google-cloud-sdk/completion.bash.inc'
+    if [ -f "$GCLOUD_HOME/completion.bash.inc" ]; then
+        source "$GCLOUD_HOME/completion.bash.inc"
     fi
 
     echo "google cloud ready."
