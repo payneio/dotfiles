24a047fbe7ee0434548a57b03040689965602aba
Paul Payne <paul@payne.io>
2019-01-09 23:04:22 -0800

    Updates python conda env

--- bash.d/python.sh
+++ bash.d/python.sh
@@ -1,13 +1,14 @@
-pip_bin="$HOME/.local/bin"
-if [ -d $pip_bin ]; then
-  pathappend "$HOME/.local/bin"
-  echo "python pip ready."
-fi
+# pip_bin="$HOME/.local/bin"
+# if [ -d $pip_bin ]; then
+#   pathappend "$HOME/.local/bin"
+#   echo "python pip ready."
+# fi
 
 conda="$HOME/miniconda3"
 if [ -d $conda ]; then
-  pathprepend "$HOME/miniconda3/bin"
-  source activate default
-  echo "python conda ready."
+    export PATH="$conda/bin:$PATH"
+    . $conda/etc/profile.d/conda.sh
+    conda activate base
+    echo "python conda ready."
 fi
 
