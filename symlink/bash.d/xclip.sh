833ca0d6f991e44fea2e3ea8a62735edb5b0a22f
Paul Payne <paul@payne.io>
2025-11-09 11:57:02 -0800

    Remove xclip (using copy/pasta now)

--- symlink/bash.d/xclip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if hash xclip 2>/dev/null; then
-  alias pbcopy='xclip -selection clipboard'
-  alias pbpaste='xclip -selection clipboard -o'
-  echo "xclip pbcopy/paste emulation ready."
-fi
-
